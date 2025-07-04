5148e2659ef8be0484b369d5909b21d0f46fa461
Destinyaifuwa <Destinyaifuwajunior@gmail.com>
2025-07-04 18:46:22 +0000

    Completed the Guess my number Game

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        // Step 1 generate a random number between 1 and 100
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 101); // 101 is not included in the list of numbers
+
+        int guess = -1; // This will initialize the value that cant't be the answer
+
+
+        Console.WriteLine("Welcome to the guess my number game!");
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess? ");
+            string input = Console.ReadLine();
+            guess = int.Parse(input);
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
